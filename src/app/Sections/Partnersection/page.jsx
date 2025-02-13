91613e4845e49991051a0a96c46357753a072ac4
umer12net <umer.net12@gmail.com>
2025-02-13 17:27:02 +0500

    partner section updated

--- src/app/Sections/Partnersection/page.jsx
+++ src/app/Sections/Partnersection/page.jsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Link from "next/link";
 
 const Partnersection = () => {
   useEffect(() => {
@@ -16,39 +17,39 @@ const Partnersection = () => {
       </h2>
       <div className="company_icons justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 py-[5%]">
         <div className="flex items-center justify-center">
-          <img
+          <Link href={"http://www.runtuchem.com/"} target="blank"><img
             className="h-auto"
             src="./../../../../partners-1.png"
             alt="Partner 1"
             data-aos="fade-up"
-          />
+          /></Link>
         </div>
         <div className="flex items-center justify-center">
-          <img
+          <Link href={"http://en.jihuadyes.com/pbl/1.html"} target="blank"><img
             className="h-auto"
             src="./../../../../partners-2.png"
             alt="Partner 2"
             data-aos="fade-up"
             data-aos-delay="100"
-          />
+          /></Link>
         </div>
         <div className="flex items-center justify-center">
-          <img
+        <Link href={"https://en.shanyu.com/"} target="blank"><img
             className="h-36"
             src="./../../../../partners-3.png"
             alt="Partner 3"
             data-aos="fade-up"
             data-aos-delay="200"
-          />
-        </div>
+            /></Link>
+            </div>
         <div className="flex items-center justify-center">
-          <img
+          <Link href={"https://www.longsheng.com/en/"} target="blank"><img
             className="h-auto"
             src="./../../../../partners-4.png"
             alt="Partner 4"
             data-aos="fade-up"
             data-aos-delay="300"
-          />
+          /></Link>
         </div>
       </div>
     </div>
