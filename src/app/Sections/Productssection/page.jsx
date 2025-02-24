ad9a0c77d1f5a177f4eb282c1c6cbed4bb27aa3f
salman24823 <salman248shah@gmail.com>
2025-02-24 18:25:38 +0500

    Update page.jsx

--- src/app/Sections/Productssection/page.jsx
+++ src/app/Sections/Productssection/page.jsx
@@ -1,28 +1,36 @@
-import { Button } from "@heroui/react";
 import Link from "next/link";
 import React from "react";
 import { IoIosArrowDropright } from "react-icons/io";
 
 const Productsection = () => {
   return (
-    <main id="products" className="Main_cnt sections p-[5%] flex items-center flex-col gap-6">
-      <h2 className="sub_heading" data-aos="fade-up">Our Products</h2>
-      <span className="text-green-900 max-[770px]:text-center text-lg font-bold" data-aos="fade-up" data-aos-delay="100">
+    <main
+      id="products"
+      className="Main_cnt sections p-[5%] flex items-center flex-col gap-6"
+    >
+      <h2 className="sub_heading" data-aos="fade-up">
+        Our Products
+      </h2>
+      <span
+        className="text-green-900 max-[770px]:text-center text-lg font-bold"
+        data-aos="fade-up"
+        data-aos-delay="100"
+      >
         Supply of high quality dyes and chemicals
       </span>
       <hr className="w-[100%]" data-aos="fade-up" data-aos-delay="200" />
       <div className="product_section grid md:grid-cols-3 grid-cols-1 gap-10">
-        <div className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/reactive.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden" data-aos="fade-up" data-aos-delay="300">
+        <div
+          className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/reactive.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
           <div className="card_content w-[100%] h-[100%] flex flex-col p-3 card_back text-white font-bold justify-between relative top-[88%]">
             <span className="text-2xl">Reactive dyes</span>
             <div className="card_icon flex flex-col items-center p-4 gap-4 ">
               <IoIosArrowDropright className="size-20" />
-              <Link
-                href={"/Products"}
-              >
-                <Button className="pr-btn">
+              <Link href={"/Products"} className="pr-btn">
                 Explore more
-                </Button>
               </Link>
               <p className="text-center">
                 It's commonly used in textile industries for dying purpose.
@@ -30,35 +38,36 @@ const Productsection = () => {
             </div>
           </div>
         </div>
-        <div className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/acrylic.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden" data-aos="fade-up" data-aos-delay="400">
+        <div
+          className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/acrylic.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="400"
+        >
           <div className="card_content w-[100%] h-[100%] flex flex-col p-3 card_back text-white font-bold justify-between relative top-[88%]">
             <span className="text-2xl">Acrylic dyes</span>
             <div className="card_icon flex flex-col items-center p-4 gap-4 ">
               <IoIosArrowDropright className="size-20" />
-              <Link
-                href={"/Products/AcrylicDyes"}
-              >
-                <Button className="pr-btn">
+              <Link href={"/Products/AcrylicDyes"} className="pr-btn">
                 Explore more
-                </Button>
               </Link>
               <p className="text-center">
-                Specially designed to colour synthetic fiber like acrylic and nylon
+                Specially designed to colour synthetic fiber like acrylic and
+                nylon
               </p>
             </div>
           </div>
         </div>
-        <div className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/sulphur.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden" data-aos="fade-up" data-aos-delay="500">
+        <div
+          className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/sulphur.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
           <div className="card_content w-[100%] h-[100%] flex flex-col p-3 card_back text-white font-bold justify-between relative top-[88%]">
             <span className="text-2xl">Sulphur dyes</span>
             <div className="card_icon flex flex-col items-center p-4 gap-4 ">
               <IoIosArrowDropright className="size-20" />
-              <Link
-                href={"/Products/SulphurDyes"}
-              >
-                <Button className="pr-btn">
+              <Link href={"/Products/SulphurDyes"} className="pr-btn">
                 Explore more
-                </Button>
               </Link>
               <p className="text-center">
                 Commonly used for cotton and other cellulose fibers.
@@ -66,57 +75,59 @@ const Productsection = () => {
             </div>
           </div>
         </div>
-        <div className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/disperse.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden" data-aos="fade-up" data-aos-delay="600">
+        <div
+          className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/disperse.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="600"
+        >
           <div className="card_content w-[100%] h-[100%] flex flex-col p-3 card_back text-white font-bold justify-between relative top-[88%]">
             <span className="text-2xl">Disperse dyes</span>
             <div className="card_icon flex flex-col items-center p-4 gap-4 ">
               <IoIosArrowDropright className="size-20" />
-              <Link
-                href={"/Products/DisperseDyes"}
-              >
-                <Button className="pr-btn">
+              <Link href={"/Products/DisperseDyes"} className="pr-btn">
                 Explore more
-                </Button>
               </Link>
               <p className="text-center">
-                Primary used for coloring synthetic fiber such as polyester and nylon.
+                Primary used for coloring synthetic fiber such as polyester and
+                nylon.
               </p>
             </div>
           </div>
         </div>
-        <div className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/hpmc.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden" data-aos="fade-up" data-aos-delay="700">
+        <div
+          className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/hpmc.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="700"
+        >
           <div className="card_content w-[100%] h-[100%] flex flex-col p-3 card_back text-white font-bold justify-between relative top-[88%]">
             <span className="text-2xl">HPMC</span>
             <div className="card_icon flex flex-col items-center p-4 gap-4 ">
               <IoIosArrowDropright className="size-20" />
-              <Link
-                href={"/Products/HPMC"}
-                
-              >
-                <Button className="pr-btn">
+              <Link href={"/Products/HPMC"} className="pr-btn">
                 Explore more
-                </Button>
               </Link>
               <p className="text-center">
-                Widely used as a thickening, binding, and stabilizing agent in industries
+                Widely used as a thickening, binding, and stabilizing agent in
+                industries
               </p>
             </div>
           </div>
         </div>
-        <div className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/direct.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden" data-aos="fade-up" data-aos-delay="800">
+        <div
+          className="product_card card1 h-[430px] w-[19rem] bg-[url(./../../public/direct.png)] bg-cover bg-no-repeat rounded-lg overflow-hidden"
+          data-aos="fade-up"
+          data-aos-delay="800"
+        >
           <div className="card_content w-[100%] h-[100%] flex flex-col p-3 card_back text-white font-bold justify-between relative top-[88%]">
             <span className="text-2xl">Direct dyes</span>
             <div className="card_icon flex flex-col items-center p-4 gap-4 ">
               <IoIosArrowDropright className="size-20" />
-              <Link
-                href={"/Products/DirectDyes"}
-              >
-                <Button className="pr-btn">
+              <Link href={"/Products/DirectDyes"} className="pr-btn">
                 Explore more
-                </Button>
               </Link>
               <p className="text-center">
-               Water soluble colorants that want directly to fiber or primary cotton.
+                Water soluble colorants that want directly to fiber or primary
+                cotton.
               </p>
             </div>
           </div>
